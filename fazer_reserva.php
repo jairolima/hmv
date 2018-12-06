0432215243aa03bbb01c1bfc3b552cf8d61ef3d5
jairolima <36547876+jairolima@users.noreply.github.com>
2018-12-06 18:51:13 -0200

    Update fazer_reserva.php

--- fazer_reserva.php
+++ fazer_reserva.php
@@ -1,50 +1,5 @@
-<?php
-    include("url_site.php");
-    include("db/dbconnect.php");
-?>
-
-<script language="JavaScript" >
-        function enviardados(){
-          
-        if(document.dados.tx_nome.value=="" || document.dados.tx_nome.value.length < 4)
-        {
-        alert( "Preencha campo NOME corretamente!" );
-        document.dados.tx_nome.focus();
-        return false;
-        }
-          
-        if( document.dados.tx_email.value=="" || document.dados.tx_email.value.indexOf('@')==-1 || document.dados.tx_email.value.indexOf('.')==-1 )
-        {
-        alert( "Preencha campo E-MAIL corretamente!" );
-        document.dados.tx_email.focus();
-        return false;
-        }
-
-        if(document.dados.tx_cpf.value=="" || document.dados.tx_cpf.value.length < 6)
-        {
-        alert( "Preencha campo CPF corretamente!" );
-        document.dados.tx_cpf.focus();
-        return false;
-        }
-        
-
-        var url_atual = window.location.href;
-        alert( "SUCESSO!" );
-        return true;
-
-        }
-          
-        </script>
-
-<br>
-<div class="container">
-    <div class="jumbotron">
-        <h2>Fazer Reservas</h2> 
-        <h5 class="text-primary" >Preencha os campos para fazer uma reserva!</h5><br>
-        <form method="post" action="reservar.php" name="dados" onSubmit="return enviardados();">
-
-             <?php
-         error_reporting(0);
+ <?php
+         
          $verifica =  mysqli_query($conexao,"SELECT ocupado FROM suite WHERE ocupado='N' AND tipo='individual';");
 
          while ($dados = mysqli_fetch_array($verifica)) {
@@ -65,7 +20,7 @@
            
 
            <?php
-           error_reporting(0);
+           
          $verificacasal =  mysqli_query($conexao,"SELECT ocupado FROM suite WHERE ocupado='N' AND tipo='Casal Simples';");
 
          while ($dadoscasal = mysqli_fetch_array($verificacasal)) {
@@ -86,7 +41,7 @@
           
 
             <?php
-            error_reporting(0);
+        
             $verificadeluxe =  mysqli_query($conexao,"SELECT ocupado FROM suite WHERE ocupado='N' AND tipo='Casal Deluxe';");
 
             while ($dadosdeluxe = mysqli_fetch_array($verificadeluxe)) {
@@ -103,19 +58,3 @@
             <input type="radio" name="tipo" value="Casal Deluxe" checked> Casal Deluxe <br>
 
             <?php } ?>
-
-            <br>Nome :<input type="text" name="nome" placeholder="Nome" id="tx_nome"/>
-            CPF :<input type="text" name="cpf" placeholder="Cpf" id="tx_cpf"/>
-            E-mail :<input type="text" name="email" placeholder="E-mail" id="tx_email"/>
-            Senha :<input type="text" name="senha" placeholder="Senha" required/><br>
-           
-            <br>Chegada:<input type="date" name="chegada" required/>
-            Saída:<input type="date" name="saida" required/>
-            Adulto(s):<input type="number" name="adulto" min="1" max="3" step="1" value="1"/>
-            Criança:<input type="number" name="crianca" min="0" max="2" step="1" value="0"/>
-            Idade da criança:<input type="number" name="idade" min="0" max="7" step="1" value="0"/><br>
-            <br><input class="btn btn-primary" type="reset" name="botao_limpar" value="Limpar"/>
-            <input class="btn btn-primary" type="submit" name="enviar" value="Reservar">
-        </form>
-    </div> 
-</div>
